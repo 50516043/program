bdc5af6a6d0a22838977f202d6b9e596de4dd531
50516043 <azumi_azumi0917@icloud.com>
2017-12-10 05:23:01 +0900

    server修正

--- server2.py
+++ server2.py
@@ -35,7 +35,7 @@ def size_request_server(word_list,s):#SIZEリクエスト
 def get_request_server(word_list,s,token_str):#GETリクエスト
     filename = word_list[1]
     print(token_str)
-    if word_list[2] == pbl2017.genkey(token_str):#トークンが一致した場合
+    #if word_list[2] == pbl2017.genkey(token_str):#トークンが一致した場合
         if word_list[3] == 'ALL':#ALL
             try:
                 f = open(filename)
@@ -63,7 +63,7 @@ def get_request_server(word_list,s,token_str):#GETリクエスト
             except:
                 print('NG 101 NO such file')
                 s.send('NG 101 NO such file\n'.encode())  
-    else:#トークンが一致しない場合
+    #else:#トークンが一致しない場合
         s.send('NG 103 Invalid hash value for file {} with {} '.format(filename,token_str).encode())
     ##修正必要あり
 def rep_request_server(word_list,s,token_str):#REPリクエスト
