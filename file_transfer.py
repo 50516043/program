b1fc2c3738534622e1a2a276c2805c0a4a69db52
50516043 <azumi_azumi0917@icloud.com>
2017-12-11 19:06:32 +0900

    a

--- file_transfer.py
+++ file_transfer.py
@@ -144,6 +144,7 @@ def get_request_ft(word_list,client_socket):
     get_request_client(input_list,client_socket,getarg)
     nextpass = nextpasslist()
     if nextpass != None:
+        print('next')
         SEND_FILE_request_next(nextpass)
         print('next')
     else:
