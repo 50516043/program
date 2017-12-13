74abd716751a50f8d058c8decc74cd2cbdfe0eda
50516043 <azumi_azumi0917@icloud.com>
2017-12-13 12:42:17 +0900

    up

--- test.py
+++ test.py
@@ -3,41 +3,53 @@
 #
 # 決められたファイルの指定されたサイズまでを転送するシステムのクライアント
 
-import sys
-from socket import *
+import math
 
-BUFSIZE = 4096
-file_name = 'received_data.dat'
-server_name = 'localhost'
-server_port = 50001
+route_list = [[0, 100, 80, 0, 0], [100, 0, 20, 15, 0 ], [80, 20, 0, 10, 15], [0, 15, 10, 0, 30], [0, 0, 15, 0, 30]] # 初期のノード間の距離のリスト
 
-def interact_with_server(s, size):
-    # 要求メッセージを作る  例: '4096' ... 4096バイト要求
-    req_msg = '{0:d}\n'.format(size)
+node_num = len(route_list) #ノードの数
 
-    # 要求メッセージの文字列をバイト列にして送信
-    s.send(req_msg.encode())
+unsearched_nodes = list(range(node_num)) # 未探索ノード
+distance = [math.inf] * node_num # ノードごとの距離のリスト
+previous_nodes = [-1] * node_num # 最短経路でそのノードのひとつ前に到達するノードのリスト
+distance[0] = 0 # 初期のノードの距離は0とする
 
-    # 書き込み用ファイルをオープンして処理
-    #   with 構文を使って書いています。
-    #   ファイル絡みの例外処理とクローズの処理は書く必要がありません
-    with open(file_name, 'wb') as f:    # 'wb' は「バイナリファイルを書き込みモードで」という意味
-        while True:
-            data = s.recv(BUFSIZE)   # BUFSIZEバイトずつ受信
-            if len(data) <= 0:  # 受信したデータがゼロなら、相手からの送信は全て終了
-                break
-            f.write(data)  # 受け取ったデータをファイルに書き込む
-    s.close()  # 最後にソケットをクローズ
+def get_target_min_index(min_index, distance, unsearched_nodes):
+    start = 0
+    while True:
+        index = distance.index(min_index, start)
+        found = index in unsearched_nodes
+        if found:
+            return index
+        else:
+            start = index + 1
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit('Usage: {0} size_in_bytes'.format(sys.argv[0]))
-    size = int(sys.argv[1])
+while(len(unsearched_nodes) != 0): #未探索ノードがなくなるまで繰り返す
+    # まず未探索ノードのうちdistanceが最小のものを選択する
+    posible_min_distance = math.inf # 最小のdistanceを見つけるための一時的なdistance。初期値は inf に設定。
+    for node_index in unsearched_nodes: # 未探索のノードのループ
+        if posible_min_distance > distance[node_index]: 
+            posible_min_distance = distance[node_index] # より小さい値が見つかれば更新
+    target_min_index = get_target_min_index(posible_min_distance, distance, unsearched_nodes) # 未探索ノードのうちで最小のindex番号を取得
+    unsearched_nodes.remove(target_min_index) # ここで探索するので未探索リストから除去
 
-    # ソケットを作ってサーバに接続
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect((server_name, server_port))
+    target_edge = route_list[target_min_index] # ターゲットになるノードからのびるエッジのリスト
+    for index, route_dis in enumerate(target_edge):
+        if route_dis != 0:
+            if distance[index] > (distance[ target_min_index] + route_dis):
+                distance[index] = distance[ target_min_index] + route_dis # 過去に設定されたdistanceよりも小さい場合はdistanceを更新
+                previous_nodes[index] =  target_min_index #　ひとつ前に到達するノードのリストも更新
 
-    interact_with_server(s, size)  # サーバとのデータのやりとりは別関数に
+# 以下で結果の表示
 
-    s.close()
+print("-----経路-----")
+previous_node = node_num - 1
+while previous_node != -1:
+    if previous_node !=0:
+        print(str(previous_node + 1) + " <- ", end='')
+    else:
+        print(str(previous_node + 1))
+    previous_node = previous_nodes[previous_node]
+
+print("-----距離-----")
+print(distance[node_num - 1])
