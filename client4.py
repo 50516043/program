244bbb94a58945be95d75274316a06e8146dd4f4
50516043 <azumi_azumi0917@icloud.com>
2017-12-15 01:36:04 +0900

    TIME

--- client4.py
+++ client4.py
@@ -11,8 +11,8 @@ from fileinput import filename
 
 #hostlist = ['pbl1','pbl2','pbl3','pbl4','pbl5']
 hostlist = ['pbl1','pbl2','pbl3','pbl4',]
-ft_port = 50000
-cl_port = 50002
+ft_port = 50002
+cl_port = 50001
 
 def receive_data(client_socket):#データ受信関数,aの長さが0のとき終了
     response_server = bytearray()
