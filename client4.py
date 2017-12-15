371f3ba555ca51e02a5f521088ac4ac9bfc67dcb
50516043 <azumi_azumi0917@icloud.com>
2017-12-15 14:29:48 +0900

    client

--- client4.py
+++ client4.py
@@ -12,7 +12,7 @@ import os.path
 
 #hostlist = ['pbl1','pbl2','pbl3','pbl4','pbl5']
 hostlist = ['pbl1','pbl2','pbl3','pbl4',]
-ft_port = 50000
+ft_port = 50002
 cl_port = 50001
 
 def receive_data(client_socket):#データ受信関数,aの長さが0のとき終了
