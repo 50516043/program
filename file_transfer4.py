3e3d4b8dcb62f6b0b4fde98d927c21a61a362fed
50516043 <azumi_azumi0917@icloud.com>
2018-01-12 04:16:07 +0900

    send関数

--- file_transfer4.py
+++ file_transfer4.py
@@ -77,9 +77,9 @@ def get_request_client(input_list,client_socket,getarg):#GETリクエスト
         sentence = 'GET {} {} PARTIAL {} {}\n'.format(input_list[1],getarg,input_list[3],input_list[4])
         print("[TO server]\n" + sentence)
         client_socket.send(sentence.encode())#サーバーへリクエスト
-        res_str_get = receive_data(client_socket)#サーバーからの応答を受信
-        res_str = res_str_get
-        print(res_str_get,'kansuunai')##################33
+        res_str = receive_data(client_socket)#サーバーからの応答を受信
+        res_str_get = res_str
+        print(res_str,'aaaaaaaaaaaaa')##################33
         print('[FROM server]\n' + res_str)
         if(res_str.split()[0] == 'OK'):
             PARTIAL_file_data = receive_data(client_socket)
