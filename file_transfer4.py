dbbe8da34c1b52c54d49b91dd2640c2935a4d8fb
50516043 <azumi_azumi0917@icloud.com>
2018-01-12 02:47:11 +0900

    send関数

--- file_transfer4.py
+++ file_transfer4.py
@@ -158,9 +158,9 @@ def get_request_ft2(word_list,client_socket):
 
 def get_request_ft(word_list,client_socket):
     #GET [filename] [ALL or PARTIAL] ([from]) ([to])
-    file_size = size_request_client(word_list[1],client_socket)
-    
-    sentence = "GET {} {} {} {}".format(word_list[1],"PARTIAL","0",file_size)
+    file_size = int(size_request_client(word_list[1],client_socket))
+    max_size = file_size -1
+    sentence = "GET {} {} {} {}".format(word_list[1],"PARTIAL","0",str(max_size))
     getarg = word_list[2]
     input_list = sentence.split()
     get_request_client(input_list,client_socket,getarg)
