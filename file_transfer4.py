16b17b6e04caa61e78f0cb1897f4131d7324d059
50516043 <azumi_azumi0917@icloud.com>
2018-01-12 04:41:21 +0900

    send関数

--- file_transfer4.py
+++ file_transfer4.py
@@ -49,7 +49,7 @@ def size_request_client(input_list,client_socket):#SIZEリクエスト
         sys.exit()
     sentence = '{} {} \n'.format("SIZE",filename)
     client_socket.send(sentence.encode())
-    res_str = receive_data(client_socket)
+    res_str = receive_data2(client_socket)
     print(res_str)
     tmp_list = res_str.split()
     return tmp_list[2]
