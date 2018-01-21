70ad0f3e2408eaed820fe5dca4866fd785aa3265
50516043 <azumi_azumi0917@icloud.com>
2018-01-22 06:37:16 +0900

    aaa

--- ft2.py
+++ ft2.py
@@ -19,7 +19,6 @@ filename = ''
 cl_port = 54901
 server_port = 54900  ##ポート番号
 res_str_get = ''
-dev = 5
 
 def receive_data(client_socket):#データ受信関数,受信したデータの長さが0のとき終了
     response_server = bytearray()
@@ -175,7 +174,7 @@ def get_request_ft(word_list,client_socket):
     #sentence = "GET {} {} {} {}".format(word_list[1],'PARTIAL','0',str(max_size))
     sentence = []
     i = 0
-    packet_size = int(max_size/dev)
+    packet_size = int(max_size/5)
     while True:
         j = i
         i += packet_size
@@ -188,7 +187,7 @@ def get_request_ft(word_list,client_socket):
     client_socket.close()
     input_list = []
     
-    for i in range(dev):##############
+    for i in range(5):##############
         s = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
         s.connect(('localhost',60623))
         input_list.append(sentence[i].split())
@@ -282,7 +281,7 @@ def interact_with_client(s):
             if nextpass != None:
                 print("next")
                 SEND_FILE_request_next(nextpass,fn)
-            elif fn == '{}.dat'.format(dev-1):
+            elif fn == '4.dat':
                 sentence = "ALL FILE RECEIVED \n"
                 print(sentence)
                 client_socket = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
