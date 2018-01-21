67088af6342d227303bc1333f8e2b839c9cadefd
50516043 <azumi_azumi0917@icloud.com>
2018-01-22 03:40:04 +0900

    aaa

--- ft2.py
+++ ft2.py
@@ -182,12 +182,10 @@ def get_request_ft(word_list,client_socket):
     client_socket.close()
     s = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     s.connect(('localhost',60623))
-    
+    input_list = []
     for fn in range(5):
-        input_list = []
-        input_list = sentence[i].split()
-        get_request_client(input_list,s,getarg)
-        del input_list
+        input_list[fn] = sentence[i].split()
+        get_request_client(input_list[fn],s,getarg)
         fn = '{}.dat'.format(i)
         shutil.copy("filedata.dat",fn)
         nextpass = nextpasslist()
