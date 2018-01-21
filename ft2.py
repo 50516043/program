79ca179a9ea6fcbcc0360879ada016fb3d8de3e5
50516043 <azumi_azumi0917@icloud.com>
2018-01-22 05:52:19 +0900

    aaa

--- ft2.py
+++ ft2.py
@@ -19,7 +19,7 @@ filename = ''
 cl_port = 54901
 server_port = 54900  ##ポート番号
 res_str_get = ''
-dev = 12
+dev = 6
 def receive_data(client_socket):#データ受信関数,受信したデータの長さが0のとき終了
     response_server = bytearray()
     while True:
