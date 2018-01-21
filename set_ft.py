d43b12b4ab484b9fe2bf6392387dd2c6d8edba43
50516043 <azumi_azumi0917@icloud.com>
2018-01-21 23:42:57 +0900

    aa

--- set_ft.py
+++ set_ft.py
@@ -10,7 +10,7 @@ ft_port = 54900
 def main():#main
 
     client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect(('pbl1, ft_port))
+    client_socket.connect(('pbl1', ft_port))
     client_socket.send('BANDWIDTH CALCULATION \n'.encode())
     client_socket.close()
             
