fffd5dcc2f9bd609a27bfbc5f46e6e95067e795e
50516043 <azumi_azumi0917@icloud.com>
2018-01-22 02:16:16 +0900

    aaa

--- ft2.py
+++ ft2.py
@@ -170,7 +170,7 @@ def get_request_ft(word_list,client_socket):
     #client_socket.close()
     #s = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     #s.connect(('localhost',60623))
-    get_request_client(input_list,s,getarg)
+    get_request_client(input_list,client_socket,getarg)
     nextpass = nextpasslist()
     if nextpass != None:
         SEND_FILE_request_next(nextpass)
